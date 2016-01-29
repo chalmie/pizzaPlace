7253934d28def37b0c9e7eefa84ced6f44ee9165
chalmie <student@epicodus.com>
2016-01-29 08:48:40 -0800

    add pizza object constructor

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function Pizza(selectedSize,selectedToppings) {
+  this.selectedSize = selectedSize;
+  this.selectedToppings = selectedToppings;
+}
+
+// Pizza.prototype.sizeCost = function(selectedSize) {
+//   var sizes = {
+//     personal: 4,
+//     small: 5,
+//     medium: 6,
+//     large: 7
+//   };
+//
+//   return sizes[selectedSize];
+// }
+//
+// Pizza.prototype.toppingsCost = function(selectedToppings) {
+//   var totalToppingsCost = 0;
+//   var toppings = {
+//     pepperoni: 1,
+//     sausage: 1,
+//     bacon: 1.5,
+//     mushrooms: 0.5,
+//     cheese: 1,
+//     squid: 2,
+//     shrimp: 2,
+//     onion: 0.5,
+//   };
+//
+//   selectedToppings.forEach(function(topping) {
+//     totalToppingsCost += toppings[topping];
+//   });
+//
+//   return totalToppingsCost;
+// }
---
+++ spec/specs.js
@@ -0,0 +1,19 @@
+describe('Pizza', function() {
+  // tests the object constructor (Pizza)
+  it('creates a pizza object with size and topping properties',function() {
+    var testPizza = new Pizza("medium","pepperoni");
+    expect(testPizza.selectedSize).to.equal("medium");
+  });
+  // // tests cost of chosen size prototype (sizeCost)
+  // it('creates a sizeCost prototype that calculates the selectedSize cost', function() {
+  //   var testPizza = new Pizza("medium","pepperoni");
+  //   expect(testPizza.sizeCost("medium")).to.equal(6);
+  // });
+  //
+  // // test cost of chosen toppings prototype (toppingsCost)
+  // it('creates a toppingsCost prototype that calculates the selectedToppings cost', function() {
+  //   var testPizza = new Pizza("medium", ["pepperoni","bacon"]);
+  //   expect(testPizza.toppingsCost(["pepperoni","bacon"])).to.equal(2.5);
+  // });
+
+});
