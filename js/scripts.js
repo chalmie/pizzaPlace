290c561a1dfc73f5675a127402a29571b04475d5
chalmie <student@epicodus.com>
2016-01-29 11:12:42 -0800

    create variables in jquery to accept user input

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,44 @@ Pizza.prototype.toppingsCost = function(selectedToppings) {
   return totalToppingsCost;
 }
 
+Pizza.prototype.pizzaCost = function(sizeCost,toppingsCost) {
+  return sizeCost + toppingsCost;
+}
+
+
+// function PizzaOrder() {
+//   this.pizzas = [];
+// }
+//
+// PizzaOrder.prototype.addPizza = function(pizza) {
+//   this.pizzas.push(pizza);
+// }
 // =============================
 // User Interface Logic
 // =============================
+
+
+$(document).ready(function() {
+  $('#pizza-order').submit(function(event) {
+    event.preventDefault();
+    // create variables for size and toppings
+    var size = $("input[name=optradio1]:checked").val();
+    var toppings = [];
+
+    // push checked toppings into toppings array
+    $("input:checkbox:checked").map(function(){
+      toppings.push($(this).val());
+    });
+
+    // create pizza object with user selected size and toppings
+    var userPizza = new Pizza(size,toppings);
+    
+
+  });
+});
+
+
+
+// var checkedValues = $('input:checkbox:checked').map(function() {
+//     return this.value;
+// }).get();
--- spec/specs.js
+++ spec/specs.js
@@ -16,4 +16,32 @@ describe('Pizza', function() {
     expect(testPizza.toppingsCost(["pepperoni","bacon"])).to.equal(2.5);
   });
 
+  // get total cost of pizza
+  it('combines size and topping cost to get total pizza cost', function() {
+    var testPizza = new Pizza("medium", ["pepperoni","bacon"]);
+    expect(testPizza.pizzaCost(6,2.5)).to.equal(8.5);
+  });
+
 });
+
+// describe('PizzaOrder', function() {
+//   it('creates an object to hold purchased pizzas in an empty array',function() {
+//     var testPizzaOrder = new PizzaOrder();
+//     expect(testPizzaOrder.pizzas).to.eql([]);
+//   });
+//   it('adds a pizza to the pizzas array',function() {
+//     var testPizzaOrder = new PizzaOrder();
+//     var testPizza = new Pizza("medium",["pepperoni","bacon"]);
+//     testPizzaOrder.addPizza(testPizza);
+//     expect(testPizzaOrder.pizzas).to.eql([testPizza]);
+//   });
+//   it('returns the price of all pizzas in the pizzas array', function() {
+//     var testPizzaOrder = new PizzaOrder();
+//     var testPizza1 = new Pizza("medium",["pepperoni","bacon"]);
+//     var testPizza2 = new Pizza("medium",["pepperoni","squid"]);
+//     testPizzaOrder.addPizza(testPizza1);
+//     testPizzaOrder.addPizza(testPizza2);
+//     expect(testPizzaOrder.grandTotal()).to.equal(17.5)
+//   });
+//
+// });
